0a3ad029a2db98af6479092edb4b48a671123c1e
junyimn <junyimn@outlook.com>
2021-06-07 22:56:22 +0100

    Couldn't sleep so decided to finish the Other page

--- src/containers/Developer/Developer.jsx
+++ src/containers/Developer/Developer.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import styles from "./Developer.module.scss";
-import TicketCard from "../../components/TicketCard"
+import TicketCard from "../../components/TicketCard";
 import team from "../../team.js";
 
 const Developer = () => {
-  
   const renderEmployees = () => {
     const employeesCard = team.map((i) => {
       if (i.role.includes("Developer") && !i.role.includes("Junior")) {
-        return <TicketCard name={i.name} role={i.role} />; 
+        return <TicketCard name={i.name} role={i.role} />;
       }
-
     });
     return employeesCard;
   };
