4906dfa0450a5b970b9fa59d1f765f4f0fc65622
junyimn <junyimn@outlook.com>
2021-06-07 20:24:43 +0100

    Finished the developer page

--- src/containers/Junior/Junior.jsx
+++ src/containers/Junior/Junior.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import styles from "./Junior.module.scss";
+import TicketCard from "../../components/TicketCard";
+import team from "../../team";
 
 const Junior = () => {
+  const renderEmployees = () => {
+    const employeesCard = team.map((i) => {
+      if (i.role.includes("Junior")) {
+        return <TicketCard name={i.name} role={i.role} />;
+      }
+    });
+    return employeesCard;
+  };
+
   return (
-    <>
-      <p>Junior works</p>
-    </>
+    <div className="App">
+      <h1>Ticket Tracker</h1>
+
+      <div className={styles.cardContainer}>{renderEmployees()}</div>
+    </div>
   );
 };
 
