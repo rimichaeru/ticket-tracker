353d9831f4f811edf73119a3f79d1c4480daf3e7
junyimn <junyimn@outlook.com>
2021-06-07 22:55:39 +0100

    Couldn't sleep so decided to finish the Other page

--- src/containers/Other/Other.jsx
+++ src/containers/Other/Other.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import styles from "./Other.module.scss";
+import TicketCard from "../../components/TicketCard";
+import team from "../../team";
 
 const Other = () => {
-  return (
-    <>
-      <p>Other works</p>
-    </>
-  );
+  const renderEmployees = () => {
+    const employeesCard = team.map((i) => {
+      if (!i.role.includes("Developer")) {
+        return <TicketCard name={i.name} role={i.role} />;
+      }
+    });
+    return employeesCard;
+  };
+
+  return <div className={styles.cardContainer}>{renderEmployees()}</div>;
 };
 
 export default Other;
